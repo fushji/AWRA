d0b61b4b2d8a19fe559e040756bf763922b968af
a.perrone <a.perrone@reply.it>
2021-08-24 11:59:45 +0200

    Delete my api key

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 
 const API_URL = 'https://api.openweathermap.org/data/2.5/';
-const API_KEY = 'f34f57608a5c1bf85a7c30aa6aa3f720';
+const API_KEY = '';
 
 
 const SEARCH_BASE_URL = `${API_URL}weather?appid=${API_KEY}&units=metric&q=`;
